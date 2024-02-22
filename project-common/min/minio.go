05943382604d8c197c4760555a8bf2893dc2dbd6
代庭玮 <dai2004105@163.com>
2024-02-22 10:00:00 +0800

    feat(common): 添加 Nacos、MinIO

--- project-common/min/minio.go
+++ project-common/min/minio.go
@@ -3,10 +3,52 @@ package min
 import (
 	"bytes"
 	"context"
+	"net/url"
+	"time"
+
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"strconv"
 )
+
+type MinioConfig struct {
+	Endpoint  string
+	AccessKey string
+	SecretKey string
+	Bucket    string
+	UseSSL    bool
+}
+
+var minioClient *MinioClient
+var bucketName string
+var endpoint string
+
+func InitMinio(cfg *MinioConfig) error {
+	client, err := New(cfg.Endpoint, cfg.AccessKey, cfg.SecretKey, cfg.UseSSL)
+	if err != nil {
+		return err
+	}
+	minioClient = client
+	bucketName = cfg.Bucket
+	endpoint = cfg.Endpoint
+
+	ctx := context.Background()
+	policy := `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"AWS":["*"]},"Action":["s3:GetObject"],"Resource":["arn:aws:s3:::` + bucketName + `/*"]}]}`
+	minioClient.c.SetBucketPolicy(ctx, bucketName, policy)
+	return nil
+}
+
+func GetBucket() string {
+	return bucketName
+}
+
+func GetMinioClient() (*MinioClient, error) {
+	return minioClient, nil
+}
+
+func GetEndpoint() string {
+	return endpoint
+}
 
 type MinioClient struct {
 	c *minio.Client
@@ -57,6 +99,29 @@ func (c *MinioClient) Compose(
 	)
 	return object, err
 }
+
+func (c *MinioClient) Delete(ctx context.Context, bucketName, objectName string) error {
+	return c.c.RemoveObject(ctx, bucketName, objectName, minio.RemoveObjectOptions{})
+}
+
+func (c *MinioClient) DeleteObjects(ctx context.Context, bucketName string, objectNames []string) error {
+	for _, objectName := range objectNames {
+		err := c.c.RemoveObject(ctx, bucketName, objectName, minio.RemoveObjectOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *MinioClient) Get(ctx context.Context, bucketName, objectName string) (*minio.Object, error) {
+	return c.c.GetObject(ctx, bucketName, objectName, minio.GetObjectOptions{})
+}
+
+func (c *MinioClient) PresignedGetObject(ctx context.Context, bucketName, objectName string, expiry time.Duration) (*url.URL, error) {
+	return c.c.PresignedGetObject(ctx, bucketName, objectName, expiry, nil)
+}
+
 func New(endpoint, accessKey, secretKey string, useSSL bool) (*MinioClient, error) {
 	minioClient, err := minio.New(endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
